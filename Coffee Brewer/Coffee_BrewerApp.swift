e6d4a44c20a9aa415f22849d16292c4f09c88c9f
Max Dubakov <max@fibery.io>
2025-02-12 08:46:42 +0100

    Initial Commit

---
+++ Coffee Brewer/Coffee_BrewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_BrewerApp.swift
+//  Coffee Brewer
+//
+//  Created by Maxim on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_BrewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
